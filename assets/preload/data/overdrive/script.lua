516c367aca09521e8a564cd3de397b54efb7117e
Deimos1234962536 <97746656+Deimos1234962536@users.noreply.github.com>
2022-10-08 21:11:35 +0300

    Add files via upload

---
+++ assets/preload/data/overdrive/script.lua
@@ -0,0 +1,135 @@
+function onStepHit() -- subtitles with lua
+	if subsEnabled then
+		if curStep == 412 then
+			if not familyFriendly then
+			triggerEvent('karaokelyrics','set','Bitch.  ');
+			triggerEvent('karaokelyrics','sung','Bitch.  ');
+			else
+			triggerEvent('karaokelyrics','set','Bench  ');
+			triggerEvent('karaokelyrics','sung','Bench  ');
+			end
+		end
+		if curStep == 420 then
+			triggerEvent('karaokelyrics','set','');
+		end
+		if curStep == 623 then
+			triggerEvent('karaokelyrics','set','KNOCK.');
+			triggerEvent('karaokelyrics','sung','KNOCK.');
+		end
+		if curStep == 627 then
+			triggerEvent('karaokelyrics','set','KNOCK. IT.');
+			triggerEvent('karaokelyrics','sung','KNOCK. IT.');
+		end
+		if curStep == 632 then
+			triggerEvent('karaokelyrics','set','KNOCK. IT. OFF!');
+			triggerEvent('karaokelyrics','sung','KNOCK. IT. OFF!');
+		end
+		if curStep == 640 then
+			triggerEvent('karaokelyrics','set','');
+		end
+		if curStep == 775 then
+			triggerEvent('karaokelyrics','set','Feeling');
+		end
+		if curStep == 779 then
+			triggerEvent('karaokelyrics','set','Feeling tough,');
+		end
+		if curStep == 784 then
+			if not familyFriendly then
+			triggerEvent('karaokelyrics','set','Feeling tough, midget?');
+			else
+			triggerEvent('karaokelyrics','set','Feeling tough, miniature person?');
+			end
+		end
+		if curStep == 793 then
+			triggerEvent('karaokelyrics','set','Do');
+		end
+		if curStep == 795 then
+			triggerEvent('karaokelyrics','set','Do you');
+		end
+		if curStep == 800 then
+			triggerEvent('karaokelyrics','set','Do you think');
+		end
+		if curStep == 803 then
+			triggerEvent('karaokelyrics','set','Do you think I have the');
+		end
+		if curStep == 808 then
+			triggerEvent('karaokelyrics','set','Do you think I have the pa');
+		end
+		if curStep == 811 then
+			triggerEvent('karaokelyrics','set','Do you think I have the patience');
+		end
+		if curStep == 813 then
+			triggerEvent('karaokelyrics','set','to deal');
+		end
+		if curStep == 816 then
+			triggerEvent('karaokelyrics','set','to deal with your');
+		end
+		if curStep == 822 then
+			if not familyFriendly then
+			triggerEvent('karaokelyrics','set','to deal with your shit?');
+			triggerEvent('karaokelyrics','sung','to deal with your shit?');
+			else
+			triggerEvent('karaokelyrics','set','to deal with your shorts?');
+			triggerEvent('karaokelyrics','sung','to deal with your shorts?');
+			end
+		end
+		if curStep == 831 then
+			triggerEvent('karaokelyrics','set','GET.');
+		end
+		if curStep == 836 then
+			triggerEvent('karaokelyrics','set','GET. OUT.');
+		end
+		if curStep == 839 then
+			triggerEvent('karaokelyrics','set','GET. OUT. OF.');
+		end
+		if curStep == 843 then
+			triggerEvent('karaokelyrics','set','GET. OUT. OF. MY.');
+		end
+		if curStep == 847 then
+			triggerEvent('karaokelyrics','set','GET. OUT. OF. MY. WAY.');
+			triggerEvent('karaokelyrics','sung','GET. OUT. OF. MY. WAY.');
+		end
+		if curStep == 855 then
+			triggerEvent('karaokelyrics','set','');
+		end
+		if curStep == 861 then
+			triggerEvent('karaokelyrics','set','Yeah!');
+		end
+		if curStep == 866 then
+			triggerEvent('karaokelyrics','set','Yeah! Yeah!');
+		end
+		if curStep == 870 then
+			triggerEvent('karaokelyrics','set','>:D');
+		end
+		if curStep == 877 then
+			triggerEvent('karaokelyrics','set','');
+		end
+		if curStep == 881 then
+			triggerEvent('karaokelyrics','set','You');
+		end
+		if curStep == 884 then
+			triggerEvent('karaokelyrics','set','You insufferable');
+		end
+		if curStep == 892 then
+			triggerEvent('karaokelyrics','set','You insufferable little-');
+		end
+		if curStep == 895 then
+			triggerEvent('karaokelyrics','set','');
+		end
+		if curStep == 956 then
+			triggerEvent('karaokelyrics','set','-LITTLE');
+		end
+		if curStep == 960 then
+			if not familyFriendly then
+			triggerEvent('karaokelyrics','set','-LITTLE SHIT!');
+			triggerEvent('karaokelyrics','sung','-LITTLE SHIT!');
+			else
+			triggerEvent('karaokelyrics','set','-LITTLE SHIP!');
+			triggerEvent('karaokelyrics','sung','-LITTLE SHIP!');
+			end
+		end
+		if curStep == 964 then
+			triggerEvent('karaokelyrics','set','');
+		end
+	end
+end
