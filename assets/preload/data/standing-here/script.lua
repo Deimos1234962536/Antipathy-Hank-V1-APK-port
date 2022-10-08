e261a31a2b0bf52d1e857b15ad62637130d98355
Deimos1234962536 <97746656+Deimos1234962536@users.noreply.github.com>
2022-10-08 21:15:20 +0300

    Add files via upload

---
+++ assets/preload/data/standing-here/script.lua
@@ -0,0 +1,37 @@
+function onStepHit() -- subtitles with lua
+	if subsEnabled then
+		if curStep == 143 then
+			triggerEvent('karaokelyrics','set','Standing here...');
+		end
+		if curStep == 183 then
+			triggerEvent('karaokelyrics','set','I realize...');
+		end
+		if curStep == 208 then
+			triggerEvent('karaokelyrics','set','You were just like me');
+		end
+		if curStep == 239 then
+			triggerEvent('karaokelyrics','set','Trying to make history');
+		end
+		if curStep == 287 then
+			triggerEvent('karaokelyrics','set','But who is to judge');
+		end
+		if curStep == 320 then
+			triggerEvent('karaokelyrics','set','The right from wrong');
+		end
+		if curStep == 351 then
+			triggerEvent('karaokelyrics','set','When our guard is down');
+		end
+		if curStep == 383 then
+			triggerEvent('karaokelyrics','set','I think we will both agree');
+		end
+		if curStep == 431 then
+			triggerEvent('karaokelyrics','set','That violence, breeds violence');
+		end
+		if curStep == 503 then
+			triggerEvent('karaokelyrics','set','But in the end...');
+		end
+		if curStep == 528 then
+			triggerEvent('karaokelyrics','set','IT HAS TO BE THIS WAY!!!!');
+		end
+	end
+end
